d13b3f708f6c045460ff48e930ba3c1256db6466
zilaanatolegregory <zilaanatolegregory6@hotmail.com>
2026-05-02 11:14:39 -0300

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bb183ae4e981979c"; }
+/******/ 		__webpack_require__.h = function() { return "5102b23d6d5a8745"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
